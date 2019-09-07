ff5fdebb6b5ed92886b153c0ccdd5b7a28e0efc6
Nikos Stanogias <nstanogias@gmail.com>
2019-09-07 15:34:12 +0200

    fixed styling, added directives, binding, ng-template

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.scss']
 })
 export class ShoppingEditComponent implements OnInit {
+  // @ts-ignore
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  // @ts-ignore
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  @Output() addedIngredient = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.addedIngredient.emit(newIngredient);
+  }
 }
