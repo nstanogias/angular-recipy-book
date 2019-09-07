0a6ea0d5b22922bd892624cb4d09269026d3b9c0
Nikos Stanogias <nstanogias@gmail.com>
2019-09-07 22:08:30 +0200

    changed eventEmitter to Subject

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -9,7 +9,6 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
   constructor(private recipeService: RecipeService,
               private router: Router,
@@ -17,10 +16,6 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
   }
 
   onAddNewRecipe() {
