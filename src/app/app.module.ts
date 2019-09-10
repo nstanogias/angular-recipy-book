6210a4d2741dc752d56637149713ea90ffe3a91b
Nikos Stanogias <nstanogias@gmail.com>
2019-09-10 20:00:57 +0200

    breaking project into modules, implemented lazy loading, preloading strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,52 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { RecipesComponent } from './recipes/recipes.component';
 import { BsDropdownModule, CollapseModule } from 'ngx-bootstrap';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {RecipeService} from './recipes/recipe.service';
 import {AppRoutingModule} from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
-    RecipesComponent,
-    ShoppingEditComponent,
-    ShoppingListComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
     BrowserAnimationsModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [
     ShoppingListService,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlertComponent } from './alert/alert.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { LoggingService } from '../logging.service';
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+  ],
+  imports: [CommonModule],
+  exports: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    CommonModule
+  ],
+  entryComponents: [AlertComponent],
+  providers: [LoggingService]
+})
+export class SharedModule {}
